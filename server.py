d2b1bda45c7f58d66e0f4bc23a7e419c3c7102e6
Izabella Dolezsar <izabella.dolezsar@gmail.com>
2019-07-29 10:28:29 +0200

    added basic files

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, redirect, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=8000,
+        debug=True,
+    )
