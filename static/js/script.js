868b4aa9437d0fde226ca7eebd5912fb59773a38
Izabella Dolezsar <izabella.dolezsar@gmail.com>
2019-07-29 14:54:21 +0200

    added User story 'Handle click event on cells: show number or bomb '

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 function main() {
     let board = Array(12).fill().map(() => Array(12).fill(0));
     placeMines(10, board);
-
+    showCellContent(board)
 
 
 
@@ -22,5 +22,22 @@ function placeMines(mineNumber, board){
     console.log(board);
 }
 
+function showCellContent(board) {
+    let gameCells = document.querySelectorAll(".unknown");
+    for (let i = 0; i < gameCells.length; i++) {
+        let gameCell = gameCells[i];
+        let row = gameCells[i].dataset.row;
+        let col = gameCells[i].dataset.col;
+        gameCell.addEventListener('click', function() {
+            gameCell.classList.add("known");
+            gameCell.classList.remove("unknown");
+            if (board[row][col] === -1) {
+                gameCell.innerHTML = "<i class=\"fas fa-bomb\"></i>";
+            } else {
+            gameCell.textContent = board[row][col];
+            }
+        });
+    }
+}
 
 main();
