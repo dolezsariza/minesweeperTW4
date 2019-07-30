625d78bbf4e65ee266c174a6def20fb29b48c225
Izabella Dolezsar <izabella.dolezsar@gmail.com>
2019-07-30 13:30:43 +0200

    added counter

--- static/js/script.js
+++ static/js/script.js
@@ -15,6 +15,11 @@ function main() {
     console.log(board);
     console.log(count);
     placeFlag();
+
+    gameOver = false; //needs the function
+     while (!gameOver) {
+        counter();
+    }
 }
 
 function placeMines(mineNumber, board){
@@ -118,5 +123,26 @@ function gameOver(){
 
 }
 
+function counter(){
+    var minutesLabel = document.getElementById("minutes");
+    var secondsLabel = document.getElementById("seconds");
+    var totalSeconds = 0;
+    setInterval(setTime, 1000);
+
+    function setTime() {
+      ++totalSeconds;
+      secondsLabel.innerHTML = pad(totalSeconds % 60);
+      minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+    }
+
+    function pad(val) {
+      var valString = val + "";
+      if (valString.length < 2) {
+        return "0" + valString;
+      } else {
+        return valString;
+      }
+    }
+}
 
 main();
